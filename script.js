479cc3495d1e0bd7e4523c6d1f1c47713ba4658c
Javier Lona <javierlona1@gmail.com>
2018-07-08 12:17:47 -0500

    cleaned up code

--- script.js
+++ script.js
@@ -1,46 +1,60 @@
 // Constant variables
 const FORM = document.querySelector('#addForm');
-const ITEMLIST = document.querySelector('.list-group');
+const TASKLIST = document.querySelector('.list-group');
 const FILTER = document.querySelector('#filter');
 
 // Event listeners
-FORM.addEventListener('submit', add_item);
-ITEMLIST.addEventListener('click', remove_item);
-FILTER.addEventListener('keyup', filter_items);
+FORM.addEventListener('submit', add_task);
+TASKLIST.addEventListener('click', remove_task);
+FILTER.addEventListener('keyup', filter_tasks);
 
 
-function add_item(event){
+function add_task(event){
   event.preventDefault();
 
   // Get input value
-  var newItem = document.querySelector('#item').value;
+  let newTask = document.querySelector('#task').value;
   // Create new li element
-  var li = document.createElement('li');
+  let liElement = document.createElement('li');
   // Create delete button element
-  var deleteBTN = document.createElement('button');
+  let deleteBTN = document.createElement('button');
 
   //  Add classes
-  li.className = 'list-group-item';
+  liElement.className = 'list-group-item';
   deleteBTN.className = 'btn btn-danger btn-sm float-right delete';
 
   // Add to DOM Tree from outside in
-  ITEMLIST.append(li);
-  li.append(newItem);
+  TASKLIST.append(liElement);
+  liElement.append(newTask);
   deleteBTN.append("X");
-  li.append(deleteBTN);
+  liElement.append(deleteBTN);
 }
 
-function remove_item(event) {
+function remove_task(event) {
   if(event.target.classList.contains('delete')){
     if(confirm("Are you sure?")){
-      var delLiItem = event.target.parentElement;
-      ITEMLIST.removeChild(delLiItem);
+      let delLiTask = event.target.parentElement;
+      TASKLIST.removeChild(delLiTask);
     }
   }
 }
 
-function filter_items(event) {
-  console.log("Here");
+function filter_tasks(event) {
+  // Grab and convert to lowercase the users search term
   let text = event.target.value.toLowerCase();
-  console.log(text);
+  // Select all the li tasks
+  let tasks = TASKLIST.querySelectorAll('li');
+
+  // Create an array of the tasks and traverse them
+  let tasksArray = Array.from(tasks);
+  for (var i = 0; i < tasksArray.length; i++) {
+    let taskName = tasksArray[i].firstChild.textContent;
+    // Check if it's a match
+    if(taskName.toLowerCase().indexOf(text) != -1){
+      tasksArray[i].style.display = 'block';
+    }
+    else{
+      tasksArray[i].style.display = 'none';
+    }
+  }
 }
